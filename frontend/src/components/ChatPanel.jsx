3e8cc7b805c6fc66cf6f467080ce1bd65cf25b03
Mango-UofA <manglam27@arizona.edu>
2026-02-08 09:44:30 -0700

    Add missing API_BASE_URL import to ChatPanel

--- frontend/src/components/ChatPanel.jsx
+++ frontend/src/components/ChatPanel.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import { API_BASE_URL } from '../config'
 
 export default function ChatPanel({level}){
   const [query, setQuery] = useState('')
